a8801ebfde071d041f607fc20087a11c0cbc02dd
jmb463 <32370337+jmb463@users.noreply.github.com>
2018-05-17 21:02:00 +0200

    Update js.js

--- js/js.js
+++ js/js.js
@@ -1,1 +1,13 @@
-var hola;
+var actScroll = null;
+
+$(document).ready(function(){
+	$("#btn_Scroll").click(function(){activarScroll();});
+	$("#btn_Sonido").click(function(){playMusica();});
+	$("#btn_Noticias").click(function(){cargarNoticias();});
+	/*$("#loading").hide();*/ //Esconde el gif al cargar el JSON
+
+}); 
+
+function activarScroll(){
+
+}
